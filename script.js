8c78bea768181da44e5d423c102ed8dd3884edca
Victor Citadin <48863419+VictorCitadin@users.noreply.github.com>
2026-02-10 14:06:52 -0300

    ajustes

--- script.js
+++ script.js
@@ -1,4 +1,47 @@
 document.addEventListener('DOMContentLoaded', () => {
+  // Lista com os nomes das fotos
+  const nomesDasFotos = [
+    "20241012_011330-COLLAGE.jpg",
+    "20241109_150422.jpg",
+    "20241109_231518.jpg",
+    "20241223_094849.jpg",
+    "20250306_222942.jpg",
+    "20250411_210552.jpg",
+    "20250517_200928.jpg",
+    "20250607_112558.jpg",
+    "20250612_212542.jpg",
+    "20250614_152641.jpg",
+    "20250712_181437.jpg",
+    "20250913_221154.jpg",
+    "20250927_122226.jpg",
+    "20250927_214941.jpg",
+    "20251102_135309.jpg",
+    "20251123_110841.jpg",
+    "20251129_175818.jpg",
+    "20251129_204145.jpg",
+    "20251227_170252.jpg",
+    "20260101_212400.jpg",
+    "20260111_010842.jpg",
+    "20260116_215544.jpg",
+    "20260124_225729.jpg",
+    "20250303093551677.jpg",
+    "20250304142734311.jpg",
+    "AISelect_20250612_084150_Photos.jpg",
+  ];
+
+  // Embaralha (randomiza) a ordem das fotos
+  nomesDasFotos.sort(() => Math.random() - 0.5);
+
+  const containerFotos = document.querySelector('.passador-de-fotos');
+  const controles = document.querySelector('.controle-passador');
+
+  // Cria as tags <img> dinamicamente
+  nomesDasFotos.forEach(nome => {
+    const img = document.createElement('img');
+    img.src = `fotos/${nome}`;
+    containerFotos.insertBefore(img, controles);
+  });
+
   const images = document.querySelectorAll('.passador-de-fotos img');
   const prevBtn = document.getElementById('prevBtn');
   const nextBtn = document.getElementById('nextBtn');
